f1827585cccd5da88a281823bd3309e327185221
Admin <Admin@TEB123008>
2023-01-18 10:02:57 +0100

    Add project files.

---
+++ Uppgift 1.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace uppgift_1_1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            // Nästa två rader kommer att skriva ut text till användare
+            Console.WriteLine("Du är nästan klar med kapitel 1.");
+            Console.WriteLine("Du har lärt dig skriva tecken som \" och \\.");
+        }
+        
+    }
+}
